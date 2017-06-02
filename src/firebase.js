06fc54558e9fdf7ccb15a70259fb9d67bd878446
Aaron DC <aaronbdc@mail.com>
2017-06-01 21:41:34 -0400

    firebase added. signup and signin links added. adding redux next.

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config ={
+    apiKey: "AIzaSyCUmW7m5yU0pi8IyGfM7QbOnIe73Osx-jY",
+    authDomain: "goalboss-5f88f.firebaseapp.com",
+    databaseURL: "https://goalboss-5f88f.firebaseio.com",
+    projectId: "goalboss-5f88f",
+    storageBucket: "goalboss-5f88f.appspot.com",
+    messagingSenderId: "110789174660"
+  };
+
+export const firebaseApp = firebase.initializeApp(config);
