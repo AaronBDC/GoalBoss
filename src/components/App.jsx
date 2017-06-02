aeda77954be16be10a99f96791d10846c8d7658b
Aaron DC <aaronbdc@mail.com>
2017-06-02 12:28:30 -0400

    DONE. Final commit after adding redux.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import '../App.css';
+import { connect } from 'react-redux';
+import { firebaseApp } from '../firebase';
+import AddGoal from './AddGoal';
+import GoalList from './GoalList';
+import CompleteGoalList from './CompleteGoalList';
 
 class App extends Component {
+  signOut(){
+    firebaseApp.auth().signOut();
+  }
   render() {
     return (
-      <div> App </div>
-
+      <div style={{margin: '5px'}}>
+        <div className="App">
+        <div className="App-title">GoalBoss</div>
+        <AddGoal />
+        <hr />
+        <h4>Goals </h4>
+        <GoalList />
+        <hr />
+        <h4>Complete Goals</h4>
+        <CompleteGoalList />
+        <hr />
+        <button
+            className="btn btn-danger"
+            onClick={() => this.signOut()}
+            >
+              Sign Out
+            </button>
+        </div>
+      </div>
     )
   }
 }
-export default App;
+function mapStateToProps(state){
+  //console.log('state', state);
+  return {}
+}
+
+export default connect(mapStateToProps, null)(App);
